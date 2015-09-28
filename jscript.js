3562c3fc39aa6dddcc01fc6e002c199758ba24ec
ivan slobozhan <ivan.slobozhan@gmail.com>
2015-09-28 16:12:19 +0300

    delete a repo

--- jscript.js
+++
@@ -1,5 +0,0 @@
-window.onscroll = function() {
-    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-    document.getElementById('showscroll').innerHTML = scrolled + "px";
-
-}
